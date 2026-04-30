b8135e2db819dd88b8030cc93bef7505c69a08f1
Jagdish Patel <jp2004108@gmail.com>
2026-04-30 22:52:50 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-// Require and call Express
 var express = require('express');
 var bodyParser = require('body-parser');
+const client = require('prom-client');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
-// use css
 app.use(express.static("public"));
+
+// 🔥 Enable metrics
+client.collectDefaultMetrics();
 
 // placeholder tasks
 var task = [];
 var complete = [];
 
-// add a task
+// routes
 app.post("/addtask", function(req, res) {
   var newTask = req.body.newtask;
   task.push(newTask);
   res.redirect("/");
 });
 
-// remove a task
 app.post("/removetask", function(req, res) {
   var completeTask = req.body.check;
+
   if (typeof completeTask === "string") {
     complete.push(completeTask);
     task.splice(task.indexOf(completeTask), 1);
-  }
-  else if (typeof completeTask === "object") {
+  } else {
     for (var i = 0; i < completeTask.length; i++) {
       complete.push(completeTask[i]);
       task.splice(task.indexOf(completeTask[i]), 1);
     }
   }
+
   res.redirect("/");
 });
 
-// get website files
 app.get("/", function (req, res) {
   res.render("index", { task: task, complete: complete });
 });
 
-// listen for connections
+// 🔥 IMPORTANT: metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// start server
 app.listen(8080, function() {
-  console.log('Testing app listening on port 8080')
+  console.log('App running on port 8080');
 });
